262bd09978c9b77b7f01fa7ca5fdfb53c141e2e3
marcin-kli <klimaszewski.m@gmail.com>
2021-03-12 12:26:55 +0000

    add classes views and templates

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Classes
 
 # Create your views here.
+
+
+def classes(request):
+    """ A view to return the classes """
+
+    classes = Classes.objects.all()
+
+    context = {
+        'classes': classes,
+    }
+
+    return render(request, 'classes/fitness_classes.html', context)
