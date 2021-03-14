8ef49afb4f42a29830e1d6b105ec6615362cd8f1
marcin-kli <klimaszewski.m@gmail.com>
2021-03-14 11:11:38 +0000

    updated context processor, fix bug with shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+from packages.models import Packages
+
 """ A contexts to return the shopping bag on all app available on all pages """
 
 
@@ -5,12 +8,23 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    package_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        package = get_object_or_404(Packages, pk=item_id)
+        total += quantity * package.price
+        package_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'package': package,
+        })
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'package_count': package_count,
     }
 
     return context
