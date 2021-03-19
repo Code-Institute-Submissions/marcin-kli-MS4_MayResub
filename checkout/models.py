5f7cb3f7516b69bf7a5ff568f9a616d998751e38
marcin-kli <klimaszewski.m@gmail.com>
2021-03-19 17:30:51 +0000

    update checkout form, add country list

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,8 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 
+from django_countries.fields import CountryField
+
 from packages.models import Packages
 
 
@@ -11,7 +13,7 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town = models.CharField(max_length=40, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
