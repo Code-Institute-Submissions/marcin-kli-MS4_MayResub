10331cd1619f3a7f14fe0fb2f63f769f99e3bee3
marcin-kli <klimaszewski.m@gmail.com>
2021-03-21 16:29:32 +0000

    add view, url and template in classes app for admin classes management

--- classes/urls.py
+++ classes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.classes, name='classes')
+    path('', views.classes, name='classes'),
+    path('add/', views.add_classes, name='add_classes'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Classes
+from .forms import ClassesForm
 
 # Create your views here.
 
@@ -14,3 +15,14 @@ def classes(request):
     }
 
     return render(request, 'classes/fitness_classes.html', context)
+
+
+def add_classes(request):
+    """ Add a classes to the store """
+    form = ClassesForm()
+    template = 'classes/add_classes.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
