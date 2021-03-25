28170bdc6129eebc898e14077b04b2e4cc31c269
marcin-kli <klimaszewski.m@gmail.com>
2021-03-25 10:28:17 +0000

    Deployment to Heroku continue

--- fitness_studio/settings.py
+++ fitness_studio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'ww^2tiv62000z8%opyruwxv2t=fr2ycvf-ub17q2vwu(c_$paw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fitnesssstudio.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'fitness_studio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
