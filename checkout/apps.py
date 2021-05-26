707e735a0fb5320af38b75d852bc1fa095ba4bdb
marcin-kli <klimaszewski.m@gmail.com>
2021-05-26 12:34:38 +0000

    Fix to long lines in .py files

--- checkout/apps.py
+++ checkout/apps.py
@@ -5,4 +5,4 @@ class CheckoutConfig(AppConfig):
     name = 'checkout'
 
     def ready(self):
-        import checkout.signals  #this is imported to get signals working
+        import checkout.signals  # this is imported to get signals working
